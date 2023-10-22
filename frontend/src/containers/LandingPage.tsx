32cc9dda0a1702c17d0dfc7fe7412dd6e87d28a7
Prateek Sane <prateeksane@Prateeks-MacBook-Pro.local>
2023-10-22 15:43:23 -0700

    created landing page. needs icons

--- frontend/src/containers/LandingPage.tsx
+++ frontend/src/containers/LandingPage.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
 const LandingPage = () => {
-    return (
-        <div>
-            <h1>Here is the landing page</h1>
+  return (
+    <>
+      <div className="title-container">
+        <div className="title-contents">
+          <h1>Unlock Your Trading Intution...</h1>
+          <p>
+            [Team Name] is the best platform to train hone your trading skills
+            and practical market knowledge by participating in fun games with
+            your friends
+          </p>
         </div>
-    )
-}
+      </div>
+      <div className="info-container">
+        <div className="info-contents">
+          <h2>Learn (icon)</h2>
+          <p>
+            Develop Practical Trading skills by competing in market games with
+            your friends!
+          </p>
+        </div>
+        <div className="info-contents">
+          <h2>Test (icon)</h2>
+          <p>Test new strategies in a real market environment</p>
+        </div>
 
-export default LandingPage
+        <div className="info-contents">
+          <h2>Read (icon)</h2>
+          <p>
+            Stay up-to-date with market trends and news with our curated reading
+            list!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
