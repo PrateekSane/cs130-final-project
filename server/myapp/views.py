8e2c9c2628c72ca7c7e94a8c184099ce4ef1ef4c
jamesg6197 <jamesg6197@gmail.com>
2023-10-28 12:06:00 -0700

    added json responses to endpoints

--- server/myapp/views.py
+++ server/myapp/views.py
@@ -1,7 +1,10 @@
 # myapp/views.py
+import json
+
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.core import serializers
 
 from .forms import CustomUserCreationForm
 from .models import *
@@ -12,16 +15,31 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
     else:
         form = CustomUserCreationForm()
 
 def create_game_view(request):
     # Create a new game object
-    new_game = Game(start_time='2023-10-27 14:00:00', end_time='2023-10-27 16:30:00')
+    try:
+        # Create a new game object
+        new_game = Game(start_time='2023-10-27 14:00:00', end_time='2023-10-27 16:30:00')
 
-    # Save the game object to the database
-    new_game.save()
+        # Save the game object to the database
+        new_game.save()
+
+        game_json = serializers.serialize("json", [new_game])
+
+        # Parse the serialized JSON
+        game_data = json.loads(game_json)[0]
+
+        # Return a success response
+        return JsonResponse(game_data)
+
+    except Exception as e:
+        # Handle any exceptions or errors
+        return JsonResponse({'error': str(e)}, status=500)
+
+    
 
 def join_game(request, game_id):
     try:
@@ -32,10 +50,14 @@ def join_game(request, game_id):
         player_profile = PlayerProfile(starting_balance=default_starting_balance, current_balance=default_starting_balance)
         player_profile.save()
 
+        payload_json = serializers.serialize("json", [game, player_profile])
+
+        payload_data = json.loads(payload_json)
+
         # Associate the player profile with the game
         game.player_profiles.add(player_profile)
 
-        return JsonResponse({'message': 'You have successfully joined the game!'})
+        return JsonResponse(payload_data)
     except Game.DoesNotExist:
         return JsonResponse({'error': 'Game not found'}, status=404)
 
