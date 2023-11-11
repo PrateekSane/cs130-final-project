177cd6f6e23282a22267da794a5c112ceb8aa185
Solomon Lo <losolomon0@gmail.com>
2023-11-10 18:44:41 -0800

    Add mock data to scoreboard

--- frontend/src/containers/Scoreboard.tsx
+++ frontend/src/containers/Scoreboard.tsx
@@ -1,9 +1,13 @@
+import GrayBox from './GrayBox'
+
 const Scoreboard = () => {
 
     return (
         <>
-            <h1>Scoreboard</h1>
-            <h2>testing</h2>
+            <GrayBox leftText="Adam" rightText='$600.29' />
+            <GrayBox leftText="Bob" rightText='$525.48' />
+            <GrayBox leftText="Charlie" rightText='$487.20' />
+            <GrayBox leftText="Danny" rightText='$378.43' />
         </>
 
     )
